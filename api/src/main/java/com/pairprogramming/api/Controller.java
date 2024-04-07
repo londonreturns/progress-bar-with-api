dd4f0b58f9c0722bfcef4faa52fe416415b1d55d
londonreturns <subedioscar.47@gmail.com>
2024-04-07 13:53:01 +0545

    multiple values

--- api/src/main/java/com/pairprogramming/api/Controller.java
+++ api/src/main/java/com/pairprogramming/api/Controller.java
@@ -1,21 +1,40 @@
 package com.pairprogramming.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class Controller {
     @GetMapping("{number}")
-    public int returnValue(@PathVariable("number") int number){
-        return number;
+    public ResponseEntity<Value> returnValue(@PathVariable("number") int number){
+        Value value = new Value();
+        value.value = number;
+        return new ResponseEntity<>(value, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public int returnValue(){
-        return Controller.randomlyGeneratedNumber();
+    public List<Value> returnValue() {
+        List<Value> values = new ArrayList<>();
+
+        // Create and add the first value
+        Value value1 = new Value();
+        value1.value = (randomlyGeneratedNumber());
+        values.add(value1);
+
+        // Create and add the second value
+        Value value2 = new Value();
+        value2.value = (randomlyGeneratedNumber());
+        values.add(value2);
+
+        return values;
     }
 
     private static int randomlyGeneratedNumber(){
---
+++ api/src/main/java/com/pairprogramming/api/Value.java
@@ -0,0 +1,5 @@
+package com.pairprogramming.api;
+
+public class Value {
+    public int value;
+}
